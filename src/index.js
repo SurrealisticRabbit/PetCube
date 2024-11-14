ca9c4af9ad40602a387b39eb80f81530bbcf226c
SurrealisticRabbit <101332174+SurrealisticRabbit@users.noreply.github.com>
2024-11-14 12:15:24 +0000

    updated logos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiCard from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: "auto",
+  [theme.breakpoints.up("sm")]: {
+    maxWidth: "450px",
+  },
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <h1>Test</h1>
-  </React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
+    <Typography variant="h2">PetCube Emulator</Typography>
+    <CssBaseline enableColorScheme />
+    <Card variant="outlined">
+      <h2>Test</h2>
+      <canvas id="myCanvas" width="200" height="100"></canvas>
+    </Card>
+  </ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
